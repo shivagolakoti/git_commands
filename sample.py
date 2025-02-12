e49bc611cc3714179b0f6ed253d78662951e65e6
shiva <golakotisiva9177@gmail.com>
2025-02-12 16:57:14 +0530

    exceptional handling

--- sample.py
+++ sample.py
@@ -1,2 +1,12 @@
-print("lol")
-print("shiva lol")
+#print("lol")
+#print("shiva lol")
+a=10
+b="z"
+try:
+    a+b
+except TypeError:  
+    print('type error')
+else:
+    print(a+b)
+finally:
+    print('exceptional handling') 
